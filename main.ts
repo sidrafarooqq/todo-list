2220bb87bffef1bab5cf28cdde35694ab10d0148
sidra farooq <162590786+sidrafarooqq@users.noreply.github.com>
2024-04-07 17:14:43 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,30 @@
+import inquirer from "inquirer"
+
+let todoList = [];
+let conditions = true;
+
+while(conditions){
+    let addTask = await inquirer.prompt([
+        {
+            name: "task",
+            type: "input",
+            message: "Enter your new task :"
+        }
+    ]);
+
+    todoList.push(addTask. task);
+    console.log(`${addTask.task} task added in todo-list succsessfully!`)
+
+let addMoretask = await inquirer.prompt([
+    {
+        name: "addmore",
+        type: "confirm",
+        message: "Do you want to add more task ?",
+        default: "false",
+    }
+]);
+conditions = addMoretask.addmore
+
+}
+console.log("your updated todo list", todoList);
+
